ddb469191f23f576bb70bec0a3709aab9ce842b3
KaiChiehHuang <kaichieh@stanford.edu>
2016-10-21 15:34:28 -0700

    Add the pipeline file

---
+++ MGPipeline.py
@@ -0,0 +1,14 @@
+# This is a pipeline for making automatic music generator #
+#---------------------------------------------------------#
+
+
+# Load MIDI samples #
+
+
+# RNN Training #
+
+
+# Output generated MIDI file #
+
+
+# Classify music genre #
